18c345e2b73a893b128749bf620c5b060ea6f5bf
CHAND BASHA SHAIK <shaik.chand.1340124@gmail.com>
2023-06-28 10:06:11 +0000

    missing-numbers-1-to-100

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// index.js
+
+const numbers = [...Array(101).keys()].slice(1) // Create an array of numbers from 1 to 100
+const missingNumber = Math.floor(Math.random() * 100) + 1 // Generate a random missing number
+const shuffledNumbers = numbers
+  .filter(num => num !== missingNumber)
+  .sort(() => Math.random() - 0.5) // Shuffle the numbers and remove the missing number
+
+console.log('Original Numbers:', numbers)
+console.log('Missing Number:', missingNumber)
+console.log('Shuffled Numbers:', shuffledNumbers)
+
+const findMissingNumber = arr => {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  const totalSum = ((arr.length + 1) * (arr.length + 2)) / 2 // Sum of numbers from 1 to 100
+  const missingNumber = totalSum - sum
+  return missingNumber
+}
+
+const foundMissingNumber = findMissingNumber(shuffledNumbers)
+console.log('Found Missing Number:', foundMissingNumber)
